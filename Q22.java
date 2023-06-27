a8eb45cd5e8ceb4053daa1d8d78ab3f09aaae96d
Alok Sharma <114926065+aloksharma10@users.noreply.github.com>
2023-06-27 08:50:56 +0530

    Update Q22.java

--- Q22.java
+++ Q22.java
@@ -1,21 +1,21 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Q22 {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/mydatabase"; 
-        String username = "your-username"; 
-        String password = "your-password"; 
-
         try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-            String insertQuery = "INSERT INTO employees (id, name, age) VALUES (?, ?, ?)";
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:Xe", "system", "admin");
+            String insertQuery = "INSERT INTO employees (id, name, age) VALUES (1, emp1, 23)";
             PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
             insertStatement.setInt(1, 1);
             insertStatement.setString(2, "John Doe");
             insertStatement.setInt(3, 30);
             insertStatement.executeUpdate();
             System.out.println("Data inserted successfully.");
-            String updateQuery = "UPDATE employees SET age = ? WHERE id = ?";
+            String updateQuery = "UPDATE employees SET age = 23 WHERE id = 1";
             PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
             updateStatement.setInt(1, 35);
             updateStatement.setInt(2, 1);
@@ -24,8 +24,8 @@ public class Q22 {
             updateStatement.close();
             insertStatement.close();
             connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Exception during connection " + e);
         }
-    }
+     }
 }
